c85a4e5b6da3935d85937d8400dbcc38255de786
Bryan Berardi <bryanberardi@cloudfitsoftware.com>
2024-10-14 09:01:17 -0400

    add version

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/finance-calculator/",
   plugins: [react()],
+  define: {
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+  },
 });
