2c80d5bc8c52e719da5357f3c18701ca323fa354
Bryan Berardi <bryanberardi@cloudfitsoftware.com>
2024-11-29 00:51:56 -0500

    add branding + layout fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,24 @@ import { Footer } from "./Footer";
 import { Body } from "./Body";
 import { Header } from "./Header";
 import { LocalizationProvider } from "@mui/x-date-pickers";
+import { Box, Stack } from "@mui/material";
 
 function App() {
   return (
     <div className="wrapper">
+
       <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Stack sx={{height: '100vh', flexDirection: 'column'}}>
         <Header />
-        <Body />
+        <Box sx={{
+          flexGrow: 1,
+          display: 'flex',
+          flexDirection: 'column'
+        }} >
+          <Body />
+        </Box>
         <Footer />
+      </Stack>
       </LocalizationProvider>
     </div>
   );
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,12 @@
+import { Stack, Typography } from "@mui/material";
+
 export const Header = () => {
   return (
     <div className="header">
-      <h2>Financial Calculator</h2>
+      <Stack spacing={2}>
+        <Typography variant="h2">PathWise</Typography>
+        <Typography variant="h5">Your financial calculator and forecastor</Typography>
+      </Stack>
     </div>
   );
 };
